6b1780343a3e09ab4ff6720b4afcb44516151c63
fkellner21 <kellner.francisco@gmail.com>
2021-08-22 13:39:04 -0300

    primerentrega

--- js/login.js
+++ js/login.js
@@ -1,6 +1,21 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+function verificar(){
+    let dato= document.getElementById("user")
+    let usuario = {};
+    if (dato.value.trim() === "")
+        {
+            alert("sin datos");
+            
+            }
+    else {
+                location.href="index.html"
+                usuario.nombre = dato.value
+                usuario.estado = "conectado"
 
-});
+                localStorage.setItem("usuario",JSON.stringify(usuario));
+                sessionStorage.setItem("usuario",JSON.stringify(usuario));
+        }
+}
+function desconectar(){
+    localStorage.clear();
+    location.href="login.html";
+}
